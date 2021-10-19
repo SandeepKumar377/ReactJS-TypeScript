c255fc16aa4fc058fbf51dc39dea66d3521c4f07
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-10-19 19:41:07 +0530

    Event handling and style props

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,62 @@
 import './App.css';
-import { Greet } from './components/Greet';
-import { Heading } from './components/Heading';
-import { Oscar } from './components/Oscar';
-import { Person } from './components/Person';
-import { PersonList } from './components/PersonList';
-import { Status } from './components/Status';
+import { Container } from './components/Container';
+// import { Button } from './components/Button';
+// import { Input } from './components/Input';
+// import { Greet } from './components/Greet';
+// import { Heading } from './components/Heading';
+// import { Oscar } from './components/Oscar';
+// import { Person } from './components/Person';
+// import { PersonList } from './components/PersonList';
+// import { Status } from './components/Status';
 
 function App() {
   // Passing simple props
-  const personName = {
-    fName: "Sandeep",
-    lName: "Kumar"
-  }
+  // const personName = {
+  //   fName: "Sandeep",
+  //   lName: "Kumar"
+  // }
   // Array Props 
-  const nameList = [
-    {
-      fName: "Sandy",
-      lName: "Kumar"
-    },
-    {
-      fName: "Deep",
-      lName: "Kumar"
-    },
-    {
-      fName: "Ravi",
-      lName: "Kumar"
-    }
-  ]
+  // const nameList = [
+  //   {
+  //     fName: "Sandy",
+  //     lName: "Kumar"
+  //   },
+  //   {
+  //     fName: "Deep",
+  //     lName: "Kumar"
+  //   },
+  //   {
+  //     fName: "Ravi",
+  //     lName: "Kumar"
+  //   }
+  // ]
   return (
     <div className="App">
-      <hr />
-      <hr />
-      <hr />
-      <Status status='error' />
+      {/* Style Props */}
+      <Container styles={{border:'1px solid red', padding:'1rem'}}/>
+
+      {/* Even handle */}
+      {/* <Button handleClick={(event, id)=>{
+        console.log('Button clicked', event, id)
+      }} />
+      <Input value='' handleChange={(event)=>console.log(event)} /> */}
+
+
+      {/* Basic props passing */}
+      {/* <Status status='error' /> 
       <Heading>Placeholder text</Heading>
       <Oscar>
         <Heading> This is Oscar.</Heading>
-      </Oscar>
+      </Oscar> */}
+
+
       {/* Optional props pass method */}
-      <Greet name='Sandeep Kumar' isLoggedIn={true} />
-      <hr />
-      {/* <Greet name='Sandeep Kumar' msaCount={24} isLoggedIn={true} /> */}
-      <hr />
-      {/* Simple props Example*/}
-      <Person name={personName} />
-      <hr />
+      {/* <Greet name='Sandeep Kumar' isLoggedIn={true} />
+      <Greet name='Sandeep Kumar' msaCount={24} isLoggedIn={true} /> */}
+      {/* Simple props Example */}
+      {/* <Person name={personName} /> */}
       {/* Array Props Example */}
-      <PersonList names={nameList} />
+      {/* <PersonList names={nameList} /> */}
     </div>
   );
 }
